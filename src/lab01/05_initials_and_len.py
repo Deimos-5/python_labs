f78cfb2ca7cc130cde6a3f5e9fbbcf3d25677ebd
Анненко Максим <m_annenko@list.ru>
2026-09-22 15:30:40 +0300

    Финальный вариант

--- src/lab01/05_initials_and_len.py
+++ src/lab01/05_initials_and_len.py
@@ -3,5 +3,5 @@ print(f"ФИО: {name}")
 words=name.split()
 inic=words[0][0]+words[1][0]+words[2][0]
 le=len(words[0])+len(words[1])+len(words[2])
-print(f"Инициалы: {inic}")
+print(f"Инициалы: {inic}.")
 print(f"Длина (символов): {le+2}")
--- src/lab01/06_labrab.py
+++ src/lab01/06_labrab.py
@@ -1,10 +1,10 @@
-n=int(input())
+n=int(input('in_1: '))
 a=0
 b=0
 for i in range(n):
-    i=input().split()
+    i=input(f'in_{i+2}: ').split()
     if i[3]=='True':
         a+=1
     else:
         b+=1
-print(a,b)
+print('out: ',a,b)
